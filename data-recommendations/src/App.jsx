a182a3a1728f928d608265da2080fe35f9a4af8e
Lye-Frozen-Heart <torresbravoalex@gmail.com>
2024-11-23 15:25:39 +0100

    Added React

---
+++ data-recommendations/src/App.jsx
@@ -0,0 +1,12 @@
+//import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div style={{ display: "flex", justifyContent: "center" }}>HOLA</div>
+    </>
+  );
+}
+
+export default App;
